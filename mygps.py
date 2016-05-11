a8318485cc21e12569ab9cd0a00f90fc7fd419f8
Jeff ilse <jli@ilseus.net>
2016-05-11 14:28:23 +0000

    gps gen

--- mygps.py
+++ mygps.py
@@ -1,33 +1,42 @@
 import gps
 import time
 import json
+import RPi.GPIO as GPIO
+import os
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)
 
-print "starting" 
-# Listen on port 2947 (gpsd) of localhost
+def writemdfile(lat, lon):
+	targetfile = open("_posts/SailTracks/"+time.strftime("%Y-%m-%d-Sailtrack-") + timestr+".md", 'w')
+	targetfile.write("---\r")
+	targetfile.write("layout: track\r")
+	targetfile.write("title: sail track "+ timestr +"\n")
+	targetfile.write("categories: sailtrack\n")
+	targetfile.write("date: " + time.strftime("%Y-%m-%d") + "\n")
+	targetfile.write("published: false\n")
+	targetfile.write("geo: " + timestr + ".json\n")
+	targetfile.write("geocenterlon:"+ str(lat) +"\n")
+	targetfile.write("geocenterlat:"+ str(lon) +"\n")
+	targetfile.write("mapzoom: 11\n")
+	targetfile.write("---\r\r")
+	targetfile.flush()
+	targetfile.close()
+	return
+def blink(wait):
+	GPIO.output(7,True)
+	time.sleep(wait)
+	GPIO.output(7,False)
+	time.sleep(wait)
+	return
+
 timestr = time.strftime("%Y-%m-%d-%H:%M:%S")
-
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
-targetfile = open("_posts/SailTracks/"+timestr+".md", 'w')
-targetfile.write("---\r")
-targetfile.write("layout: track\r")
-targetfile.write("title: sail track\n")
-targetfile.write("categories: sailtrack\n")
-targetfile.write("date: " + timestr + "\n")
-targetfile.write("published: false\n")
-targetfile.write("geo: " + timestr + ".json\n")
-targetfile.write("geocenterlon:\n")
-targetfile.write("geocenterlat:\n")
-targetfile.write("mapzoom: 11\n")
-targetfile.write("---\r\r")
-targetfile.close()
-
-
 firstrun = True
 lasttime = ""
 firsttime = ""
 cachefilepath = "sailtrack/" + timestr + "-cache.json"
-cachefile = open(cachefilepath, 'w')
+cachefile = open(cachefilepath, 'w', 1)
 
 while True:
 	try:
@@ -38,10 +47,12 @@ while True:
 				lasttime = report.time
 				if firstrun == True:
 					firsttime = report.time
+					writemdfile(report.lat, report.lon)
 					firstrun = False
+					
 				cachefile.write(str(report.lon) + ",")
 				cachefile.write(str(report.lat) + "\n")
-				print "write"
+				blink(1)
 	except KeyError:
 		pass
 	except KeyboardInterrupt:
@@ -49,9 +60,9 @@ while True:
 	except StopIteration:
 		session = None		
 
-
+cachefile.flush()
 cachefile.close()
-datafile = open("sailtrack/" + timestr + ".json", 'w')
+datafile = open("sailtrack/" + timestr + ".json", 'w', 500)
 f = open("sailtracktemplate.json", 'r')
 json_data = f.read()
 f.close()
@@ -68,5 +79,7 @@ jd["features"][0]["properties"]["start"] = firsttime
 jd["features"][0]["properties"]["end"] = lasttime
 
 datafile.write(json.dumps(jd, indent=4))
+datafile.flush()
 datafile.close()
-
+GPIO.cleanup()
+os.remove(cachefilepath)
